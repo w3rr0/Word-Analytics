afcf8a6341d60af5c84d410ee97191be681cba15
w3rr0 <konradmateja65@gmail.com>
2024-11-29 13:43:36 +0100

    fix stats table look

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,7 +1,22 @@
 export default function Stats() {
   return (
     <section className="stats">
-      Stats
+      <section className="stat">
+        <span className="stat__number">0</span>
+        <h2 className="second-heading">Words</h2>
+      </section>
+      <section className="stat">
+        <span className="stat__number">0</span>
+        <h2 className="second-heading">Characters</h2>
+      </section>
+      <section className="stat">
+        <span className="stat__number">280</span>
+        <h2 className="second-heading">Instagram</h2>
+      </section>
+      <section className="stat">
+        <span className="stat__number">2200</span>
+        <h2 className="second-heading">Facebook</h2>
+      </section>
     </section>
   )
 }
