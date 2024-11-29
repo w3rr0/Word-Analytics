6e2a2c03e1f9d59e0abca2410d86e56d94fad6e8
w3rr0 <konradmateja65@gmail.com>
2024-11-29 13:30:11 +0100

    usunięcie zmiennej warning_message - wyswietla sie caly czas tylko jako pusty string

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -3,18 +3,15 @@ import Warning from "./Warning";
 
 export default function Textarea() {
 const [text, setText] = useState ("")
-const [warning, setWarning] = useState(false)
 const [message, setMessage] = useState("")
 
 const changeHandler = (e) => {
     let newText = e.target.value
     if (newText.includes("<script>")) {
         setMessage("No script tags allowed!");
-        setWarning(true);
         newText = newText.replace("<script>", "");
     } else if (newText.includes("@")) {
         setMessage("No @ symbol allowed!");
-        setWarning(true);
         newText = newText.replace("@", "");
     }
     setText(newText)
@@ -27,7 +24,7 @@ const changeHandler = (e) => {
         onChange={changeHandler}
         placeholder="Enter your text"
         spellCheck="false"/>
-        {warning ? <Warning message={message}/> : null}
+        <Warning message={message}/>
         </div>
         )
     }
